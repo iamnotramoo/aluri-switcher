80359369f07686f4de72b8e6150dc248e089d909
iamnotramoo <59285081+iamnotramoo@users.noreply.github.com>
2020-03-13 12:36:14 -0700

    Switcher fully translated to English?
    I don't know if this switcher is fully translated. Maybe not? Just translated this today

---
+++ switcher-master/Switcher/Switcher/tr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switcher
+{
+    class tr
+    {
+        //NO API, NO,NO
+        public static string minaseIP = "35.228.101.58";
+
+        public static string onminase = "You are now connected and playing on osu!aluri";
+
+
+        public static string onoff = "You are currently connected and playing on official server.";
+
+        public static string switchtominase = "Switch to osu!aluri";
+
+        public static string switchtooff = "Switch to official osu! server.";
+    }
+}
